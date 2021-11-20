03219dc2276a410ced9621ef6a59536b402806e8
azhari <azharisaputrasikumbang@gmail.com>
2021-11-20 17:31:15 +0700

    add admin panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,20 +17,20 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-50">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
+            <div class="flex overflow-hidden bg-white pt-16">
+                @include('layouts.sidebar')
+            </div>
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <div class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+                <main>
+                    <div class="pt-6 px-4"> 
+                        {{ $slot }}
+                    </div>
+                </main>
+            </div>
         </div>
     </body>
 </html>

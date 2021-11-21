d95052a19790466617fe05a4f0c25d02fb254e6d
azhari <azharisaputrasikumbang@gmail.com>
2021-11-21 21:38:50 +0700

    add lang (id)

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,7 @@
+<?php 
+
+return [
+	'failed'   => 'Identitas tersebut tidak cocok dengan data kami.',
+	'password' => 'Password yang anda masukkan salah.',
+	'throttle' => 'Terlalu banyak usaha masuk. Silahkan coba lagi dalam :seconds detik.',
+];
